bc30eabdda2ca434c551fe99f09934804b708738
knowmaker <59835668+knowmaker@users.noreply.github.com>
2026-02-02 23:08:29 +0300

    added CORS

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 # app/main.py
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.api.v1 import lt, gk, users, quantities, law_groups, laws, represents, system_types
 
@@ -7,6 +8,20 @@ app = FastAPI(
     title="FViZ Backend",
     version="0.1.0",
 )
+
+origins = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,          # или ["*"] для полного разрешения (не советую для прод)
+    allow_credentials=True,         # нужно если используешь cookies/authorization
+    allow_methods=["*"],            # GET/POST/PATCH/DELETE...
+    allow_headers=["*"],            # Authorization, Content-Type и т.п.
+)
+
 
 app.include_router(lt.router, prefix="/lt", tags=["lt"])
 app.include_router(system_types.router, prefix="/system_types", tags=["system_types"])
